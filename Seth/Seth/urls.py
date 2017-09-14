95cc9c65e9a697bdf1825abb707f2bac8d39b4c0
Martijn Verkleij <github@mverkleij.nl>
2017-09-14 16:17:01 +0200

    Added static file sitelogo

--- Seth/Seth/urls.py
+++ Seth/Seth/urls.py
@@ -1,3 +1,7 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 """Seth URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -18,4 +22,4 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
