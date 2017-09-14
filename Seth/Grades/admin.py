c745193f1bfd568691d38ef488feb3b5848e14a7
Martijn Verkleij <github@mverkleij.nl>
2017-09-14 11:50:03 +0200

    Added studying to admin dashboard

--- Seth/Grades/admin.py
+++ Seth/Grades/admin.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 # Register your models here.
 from Grades.models import *
 
-for object in [Study, Module, Module_ed, Criterion, Course, Test, Grade, Student, Teacher, Advisor]:
+for object in [Study, Studying, Module, Module_ed, Criterion, Course, Test, Grade, Student, Teacher, Advisor]:
     admin.site.register(object)
