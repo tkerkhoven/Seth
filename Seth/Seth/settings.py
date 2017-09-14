81c42b957c9517f76bab4a498a4f622790022b9d
Martijn Verkleij <github@mverkleij.nl>
2017-09-14 10:58:03 +0200

    Added Dashboard App, added routes for index page, added index page, added template for login

--- Seth/Seth/settings.py
+++ Seth/Seth/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = '/login/'
