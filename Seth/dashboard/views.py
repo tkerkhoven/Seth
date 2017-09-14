72d4378de264a5ae3eed6c54e8e29761b2a59365
Gerwin <gerwinputtenstein@gmail.com>
2017-09-14 16:22:13 +0200

    Update on mockup dashboard

--- Seth/dashboard/views.py
+++ Seth/dashboard/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 
 
-@permission_required('grades.add_grade')
+# @permission_required('grades.add_grade')
 def home(request):
     context = dict()
     return render(request, 'dashboard/index.html', context)
