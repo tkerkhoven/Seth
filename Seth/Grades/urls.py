2ccf614f2336b5292e972952af869338beb8077a
Kevkev1 <kevin1226@live.nl>
2017-09-19 17:21:37 +0200

    Started on student grade overviews

--- Seth/Grades/urls.py
+++ Seth/Grades/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'grades'
 urlpatterns = [
 	url(r'^$', views.ModuleView.as_view(), name='modules'),
-    url(r'^(?P<pk>([0-9]+))/$', views.GradeView.as_view(), name='gradebook'),
-    url(r'^(?P<pk>([0-9]+))/(?P<sid>([0-9]+))/$', views.StudentView.as_view(), name='student'),
+    url(r'^modules/(?P<pk>([0-9]+))/$', views.GradeView.as_view(), name='gradebook'),
+    url(r'^modules/(?P<pk>([0-9]+))/(?P<sid>([0-9]+))/$', views.ModuleStudentView.as_view(), name='modstudent'),
+    url(r'^students/(?P<pk>([0-9]+))/$', views.StudentView.as_view(), name='student'),
 ]
--- Seth/Grades/views.py
+++ Seth/Grades/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from django.views import generic
-from .models import Module, Studying, Person, Module_ed, Course, Test, Grade
+from .models import Module, Studying, Person, Module_ed, Test, Grade
+from django.contrib.auth.models import User
 
 
 class ModuleView(generic.ListView):
@@ -9,7 +9,6 @@ class ModuleView(generic.ListView):
 
     def get_queryset(self):
         return Module.objects.order_by('name')
-
 
 class GradeView(generic.DetailView):
     template_name = 'Grades/gradebook.html'
@@ -22,14 +21,13 @@ class GradeView(generic.DetailView):
         course_dict = dict()
         test_dict = dict()
 
-        mod = Module_ed.objects.get(module=self.kwargs['pk'])
-        for studying in Studying.objects.all().filter(module_id=mod.id):
+        mod_ed = Module_ed.objects.get(module=self.kwargs['pk'])
+        for studying in Studying.objects.all().filter(module_id=mod_ed.id):
             student_dict = dict()
 
-            student_dict['id'] = studying.student_id.person_id
+            student_dict['id'] = studying.student_id.user.id
             student_dict['user'] = studying.student_id.user
-            print(studying.student_id.user)
-            for course in mod.courses.all():
+            for course in mod_ed.courses.all():
 
                 test_list = []
                 if course.id not in course_dict.keys():
@@ -51,22 +49,48 @@ class GradeView(generic.DetailView):
         context['testdict'] = test_dict
         return context
 
-
 class StudentView(generic.DetailView):
     template_name = 'Grades/student.html'
-    model = Module
+    model = User
 
     def get_context_data(self, **kwargs):
         context = super(StudentView, self).get_context_data(**kwargs)
 
-        mod = Module_ed.objects.get(module=self.kwargs['pk'])
+        grade_dict = dict()
+        test_dict = dict()
+        course_dict = dict()
+
+        person = Person.objects.get(user=self.kwargs['pk'])
+        for grade in Grade.objects.all().filter(student_id=person):
+            if grade.test_id.course_id not in test_dict.keys():
+                test_dict[grade.test_id.course_id] = []
+
+            grade_dict[grade.test_id] = grade.grade
+            test_dict[grade.test_id.course_id].append(grade.test_id)
+            course_dict[grade.test_id.course_id] = grade.test_id.course_id.name
+
+        context['student'] = True
+        context['gradedict'] = grade_dict
+        context['testdict'] = test_dict
+        context['coursedict'] = course_dict
+
+        return context
+
+class ModuleStudentView(generic.DetailView):
+    template_name = 'Grades/student.html'
+    model = Module
+
+    def get_context_data(self, **kwargs):
+        context = super(ModuleStudentView, self).get_context_data(**kwargs)
+
+        mod_ed = Module_ed.objects.get(module=self.kwargs['pk'])
         student = Person.objects.get(person_id=self.kwargs['sid'])
 
         course_dict = dict()
         test_dict = dict()
         grade_dict = dict()
 
-        for course in mod.courses.all():
+        for course in mod_ed.courses.all():
             test_list = []
 
             if course.id not in course_dict.keys():
@@ -77,13 +101,17 @@ class StudentView(generic.DetailView):
                 if test not in test_list:
                     test_list.append(test)
 
+                print(test)
                 for grade in Grade.objects.all().filter(student_id=student.person_id).filter(test_id=test):
                     grade_dict[test.id] = grade.grade
 
                 test_dict[course.id] = test_list
 
+        context['module_student'] = True
         context['student'] = student
         context['user'] = student.user
         context['coursedict'] = course_dict
         context['testdict'] = test_dict
         context['gradedict'] = grade_dict
+
+        return context
