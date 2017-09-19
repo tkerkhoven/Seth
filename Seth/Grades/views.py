c6318436f8974e9b0cee143fd7d6f233e8d0784b
Kevkev1 <kevin1226@live.nl>
2017-09-19 11:41:10 +0200

    Fixed some errors, cleaned up some code

--- Seth/Grades/views.py
+++ Seth/Grades/views.py
@@ -1,89 +1,89 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Module, Studying, Module_ed, Course, Test, Grade
+from .models import Module, Studying, Person, Module_ed, Course, Test, Grade
+
 
 class ModuleView(generic.ListView):
-	template_name = 'Grades/modules.html'
-	model = Module
+    template_name = 'Grades/modules.html'
+    model = Module
 
-	def get_queryset(self):
-		return Module.objects.order_by('name')
+    def get_queryset(self):
+        return Module.objects.order_by('name')
+
 
 class GradeView(generic.DetailView):
-	template_name = 'Grades/gradebook.html'
-	model = Module
+    template_name = 'Grades/gradebook.html'
+    model = Module
 
-	def get_context_data(self, **kwargs):
-		context = super(GradeView, self).get_context_data(**kwargs)
-        
-		dList = []
-		tList = []
-		courseDict = dict()
-		testDict = dict()
+    def get_context_data(self, **kwargs):
+        context = super(GradeView, self).get_context_data(**kwargs)
 
-		m_id = Module_ed.objects.get(module=self.kwargs['pk'])
-		for studying in Studying.objects.all().filter(module_id=m_id):
-			studentDict = dict()
+        student_list = []
+        course_dict = dict()
+        test_dict = dict()
 
-			studentDict['id'] = studying.student_id.person_id
-			studentDict['user'] = studying.student_id.user
-			print(studying.student_id.user)
-			for course in m_id.courses.all():
+        mod = Module_ed.objects.get(module=self.kwargs['pk'])
+        for studying in Studying.objects.all().filter(module_id=mod.id):
+            student_dict = dict()
 
-				tList = []
-				if course.id not in courseDict.keys():
-					courseDict[course.id] = course.name
+            student_dict['id'] = studying.student_id.person_id
+            student_dict['user'] = studying.student_id.user
+            print(studying.student_id.user)
+            for course in mod.courses.all():
 
-				for test in Test.objects.all().filter(course_id=course):
+                test_list = []
+                if course.id not in course_dict.keys():
+                    course_dict[course.id] = course.name
 
-					if test not in tList:
-						tList.append(test)
+                for test in Test.objects.all().filter(course_id=course):
 
-					for grade in Grade.objects.all().filter(student_id=studying.student_id).filter(test_id=test):
-						studentDict[test.id] = grade.grade
-					testDict[course.id] = tList
+                    if test not in test_list:
+                        test_list.append(test)
 
-			dList.append(studentDict)
+                    for grade in Grade.objects.all().filter(student_id=studying.student_id).filter(test_id=test):
+                        student_dict[test.id] = grade.grade
+                    test_dict[course.id] = test_list
 
-		context['studentdict'] = dList
-		context['coursedict'] = courseDict
-		context['testdict'] = testDict
-		return context
+            student_list.append(student_dict)
+
+        context['studentdict'] = student_list
+        context['coursedict'] = course_dict
+        context['testdict'] = test_dict
+        return context
+
 
 class StudentView(generic.DetailView):
-	template_name = 'Grades/student.html'
-	model = Course
+    template_name = 'Grades/student.html'
+    model = Module
 
-	def get_context_data(self, **kwargs):
-		context = super(StudentView, self).get_context_data(**kwargs)
+    def get_context_data(self, **kwargs):
+        context = super(StudentView, self).get_context_data(**kwargs)
 
-		mod = Module_ed.objects.get(module=self.kwargs['pk'])
-		stu = Student.objects.get(student_id=self.kwargs['sid'])
+        mod = Module_ed.objects.get(module=self.kwargs['pk'])
+        student = Person.objects.get(person_id=self.kwargs['sid'])
 
-		tList = []
-		courseDict = dict()
-		testDict = dict()
-		gradeDict = dict()
+        course_dict = dict()
+        test_dict = dict()
+        grade_dict = dict()
 
-		for course in mod.courses.all():
-			tList = []
+        for course in mod.courses.all():
+            test_list = []
 
-			if course.id not in courseDict.keys():
-				courseDict[course.id] = course.name	
+            if course.id not in course_dict.keys():
+                course_dict[course.id] = course.name
 
-			for test in Test.objects.all().filer(course_id=course):
+            for test in Test.objects.all().filter(course_id=course):
 
-				if test not in tList:
-					tList.append(test)
+                if test not in test_list:
+                    test_list.append(test)
 
-				for grade in Grade.objects.all().filter(student_id=studying.student_id).filter(test_id=test):
-					gradeDict[test.id] = grade.grade
+                for grade in Grade.objects.all().filter(student_id=student.person_id).filter(test_id=test):
+                    grade_dict[test.id] = grade.grade
 
-				testDict[course.id] = tList
+                test_dict[course.id] = test_list
 
-		context['student'] = stu
-		context['user'] = stu.user
-		context['coursedict'] = courseDict
-		context['testdict'] = testDict
-		context['gradedict'] = gradeDict
-
+        context['student'] = student
+        context['user'] = student.user
+        context['coursedict'] = course_dict
+        context['testdict'] = test_dict
+        context['gradedict'] = grade_dict
