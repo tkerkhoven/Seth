917ec2a11ff61c247c2c95933bc490938b60877a
Tim Kerkhoven <t.kerkhoven@student.utwente.nl>
2017-09-21 15:45:06 +0200

    updated urls and added views

--- Seth/module_management/views.py
+++ Seth/module_management/views.py
@@ -1,20 +1,10 @@
-from django.contrib.auth.decorators import user_passes_test, login_required
 from django.core.exceptions import PermissionDenied
-from django.utils.decorators import method_decorator
+from django.db.models import Q
 from django.views import generic
 
-from Grades.models import Module, Module_ed
+from Grades.models import Module, Module_ed, Course, Test
 
 
-def is_module_coordinator_check(user):
-    if not Module_ed.objects.filter(module_coordinator__user=user):
-        return False
-    else:
-        return True
-
-
-@method_decorator(login_required, name='dispatch')
-@method_decorator(user_passes_test(is_module_coordinator_check), name='dispatch')
 class IndexView(generic.ListView):
     template_name = 'module_management/index.html'
     context_object_name = 'module_list'
@@ -24,6 +14,21 @@ class IndexView(generic.ListView):
         user = self.request.user
         module_list = Module.objects.prefetch_related('module_ed_set').filter(module_ed__module_coordinator__user=user)
         return module_list
+
+    def dispatch(self, request, *args, **kwargs):
+        user = request.user
+
+        if not Module_ed.objects.filter(module_coordinator__user=user):
+            raise PermissionDenied()
+
+        # Try to dispatch to the right method; if a method doesn't exist,
+        # defer to the error handler. Also defer to the error handler if the
+        # request method isn't on the approved list.
+        if request.method.lower() in self.http_method_names:
+            handler = getattr(self, request.method.lower(), self.http_method_not_allowed)
+        else:
+            handler = self.http_method_not_allowed
+        return handler(request, *args, **kwargs)
 
 
 class ModuleEdView(generic.DetailView):
@@ -56,6 +61,48 @@ class ModuleView(generic.DetailView):
         user = request.user
 
         if not Module.objects.filter(pk=pk).filter(module_ed__module_coordinator__user=user):
+            raise PermissionDenied()
+
+        # Try to dispatch to the right method; if a method doesn't exist,
+        # defer to the error handler. Also defer to the error handler if the
+        # request method isn't on the approved list.
+        if request.method.lower() in self.http_method_names:
+            handler = getattr(self, request.method.lower(), self.http_method_not_allowed)
+        else:
+            handler = self.http_method_not_allowed
+        return handler(request, *args, **kwargs)
+
+
+class CourseView(generic.DetailView):
+    template_name = 'module_management/course_detail.html'
+    model = Course
+
+    def dispatch(self, request, *args, **kwargs):
+        pk = request.path_info.split('/')[2]
+        user = request.user
+
+        if not Course.objects.filter(Q(pk=pk) & (Q(module_ed__module_coordinator__user=user) | Q(teachers__user=user))):
+            raise PermissionDenied()
+
+        # Try to dispatch to the right method; if a method doesn't exist,
+        # defer to the error handler. Also defer to the error handler if the
+        # request method isn't on the approved list.
+        if request.method.lower() in self.http_method_names:
+            handler = getattr(self, request.method.lower(), self.http_method_not_allowed)
+        else:
+            handler = self.http_method_not_allowed
+        return handler(request, *args, **kwargs)
+
+
+class TestView(generic.DetailView):
+    template_name = 'module_management/test_detail.html'
+    model = Test
+
+    def dispatch(self, request, *args, **kwargs):
+        pk = request.path_info.split('/')[2]
+        user = request.user
+        if not Test.objects.filter(Q(pk=pk) & (
+                    Q(course_id__module_ed__module_coordinator__user=user) | Q(course_id__teachers__user=user))):
             raise PermissionDenied()
 
         # Try to dispatch to the right method; if a method doesn't exist,
