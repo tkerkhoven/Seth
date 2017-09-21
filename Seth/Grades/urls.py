f4cb1b44e46e99c160af654b76e84a25c358b5c8
JaspervanRooijen <jasper.van.rooijen@hotmail.com>
2017-09-21 15:23:29 +0200

    Basis functionality for Human Resources view is present; Users view added (overview); User view added (detailed view); Updating user functionality is present, but ugly

--- Seth/Grades/urls.py
+++ Seth/Grades/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 app_name = 'grades'
 urlpatterns = [
-	url(r'^$', views.ModuleView.as_view(), name='modules'),
+    url(r'^$', views.ModuleView.as_view(), name='modules'),
     url(r'^(?P<pk>([0-9]+))/$', views.GradeView.as_view(), name='gradebook'),
     url(r'^(?P<pk>([0-9]+)?P<sid>([0-9]+))/$', views.StudentView.as_view(), name='student'),
+    url(r'^users/$', views.PersonsView.as_view(), name='users'),
+    url(r'^user/(?P<pk>([0-9]+))/$', views.PersonDetailView.as_view(), name='user'),
+    url(r'^user/(?P<pk>([0-9]+))/update-user/$', views.UpdateUser.as_view(), name="update_user"),
 ]
--- Seth/Grades/views.py
+++ Seth/Grades/views.py
@@ -1,89 +1,157 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Module, Studying, Module_ed, Course, Test, Grade
+from .models import Module, Studying, Module_ed, Course, Test, Grade, Person
+from django.urls import reverse_lazy
+
 
 class ModuleView(generic.ListView):
-	template_name = 'Grades/modules.html'
-	model = Module
+    template_name = 'Grades/modules.html'
+    model = Module
 
-	def get_queryset(self):
-		return Module.objects.order_by('name')
+    def get_queryset(self):
+        return Module.objects.order_by('name')
+
 
 class GradeView(generic.DetailView):
-	template_name = 'Grades/gradebook.html'
-	model = Module
+    template_name = 'Grades/gradebook.html'
+    model = Module
 
-	def get_context_data(self, **kwargs):
-		context = super(GradeView, self).get_context_data(**kwargs)
-        
-		dList = []
-		tList = []
-		courseDict = dict()
-		testDict = dict()
+    def get_context_data(self, **kwargs):
+        context = super(GradeView, self).get_context_data(**kwargs)
 
-		m_id = Module_ed.objects.get(module=self.kwargs['pk'])
-		for studying in Studying.objects.all().filter(module_id=m_id):
-			studentDict = dict()
+        dList = []
+        tList = []
+        courseDict = dict()
+        testDict = dict()
 
-			studentDict['id'] = studying.student_id
-			studentDict['user'] = studying.student_id.user
-			print(studying.student_id.user)
-			for course in m_id.courses.all():
+        m_id = Module_ed.objects.get(module=self.kwargs['pk'])
+        for studying in Studying.objects.all().filter(module_id=m_id):
+            studentDict = dict()
 
-				tList = []
-				if course.id not in courseDict.keys():
-					courseDict[course.id] = course.name
+            studentDict['id'] = studying.student_id
+            studentDict['user'] = studying.student_id.user
+            print(studying.student_id.user)
+            for course in m_id.courses.all():
 
-				for test in Test.objects.all().filter(course_id=course):
+                tList = []
+                if course.id not in courseDict.keys():
+                    courseDict[course.id] = course.name
 
-					if test not in tList:
-						tList.append(test)
+                for test in Test.objects.all().filter(course_id=course):
 
-					for grade in Grade.objects.all().filter(student_id=studying.student_id).filter(test_id=test):
-						studentDict[test.id] = grade.grade
-					testDict[course.id] = tList
+                    if test not in tList:
+                        tList.append(test)
 
-			dList.append(studentDict)
+                    for grade in Grade.objects.all().filter(student_id=studying.student_id).filter(test_id=test):
+                        studentDict[test.id] = grade.grade
+                    testDict[course.id] = tList
 
-		context['studentdict'] = dList
-		context['coursedict'] = courseDict
-		context['testdict'] = testDict
-		return context
+            dList.append(studentDict)
+
+        context['studentdict'] = dList
+        context['coursedict'] = courseDict
+        context['testdict'] = testDict
+        return context
+
 
 class StudentView(generic.DetailView):
-	template_name = 'Grades/student.html'
-	model = Course
+    template_name = 'Grades/student.html'
+    model = Course
 
-	def get_context_data(self, **kwargs):
-		context = super(StudentView, self).get_context_data(**kwargs)
+    def get_context_data(self, **kwargs):
+        context = super(StudentView, self).get_context_data(**kwargs)
 
-		mod = Module_ed.objects.get(module=self.kwargs['pk'])
-		stu = Student.objects.get(student_id=self.kwargs['sid'])
+        mod = Module_ed.objects.get(module=self.kwargs['pk'])
+        stu = Student.objects.get(student_id=self.kwargs['sid'])
 
-		tList = []
-		courseDict = dict()
-		testDict = dict()
-		gradeDict = dict()
+        tList = []
+        courseDict = dict()
+        testDict = dict()
+        gradeDict = dict()
 
-		for course in mod.courses.all():
-			tList = []
+        for course in mod.courses.all():
+            tList = []
 
-			if course.id not in courseDict.keys():
-				courseDict[course.id] = course.name	
+            if course.id not in courseDict.keys():
+                courseDict[course.id] = course.name
 
-			for test in Test.objects.all().filer(course_id=course):
+            for test in Test.objects.all().filer(course_id=course):
 
-				if test not in tList:
-					tList.append(test)
+                if test not in tList:
+                    tList.append(test)
 
-				for grade in Grade.objects.all().filter(student_id=studying.student_id).filter(test_id=test):
-					gradeDict[test.id] = grade.grade
+                for grade in Grade.objects.all().filter(student_id=studying.student_id).filter(test_id=test):
+                    gradeDict[test.id] = grade.grade
 
-				testDict[course.id] = tList
+                testDict[course.id] = tList
 
-		context['student'] = stu
-		context['user'] = stu.user
-		context['coursedict'] = courseDict
-		context['testdict'] = testDict
-		context['gradedict'] = gradeDict
+        context['student'] = stu
+        context['user'] = stu.user
+        context['coursedict'] = courseDict
+        context['testdict'] = testDict
+        context['gradedict'] = gradeDict
 
+
+class PersonsView(generic.ListView):
+    template_name = 'Grades/users.html'
+    model = Person
+
+    def get_context_data(self, **kwargs):
+        context = super(PersonsView, self).get_context_data(**kwargs)
+
+        persons = Person.objects.all()
+        person_dict = dict()
+        for person in persons.all():
+            data = dict()
+            data['name'] = person.name
+            data['role'] = person.role
+            data['full_id'] = person.full_id
+            person_dict[person.id] = data
+            print(person.name)
+        context['persons'] = person_dict
+        return context
+
+
+class PersonDetailView(generic.DetailView):
+    template_name = 'Grades/user.html'
+    model = Person
+
+    def get_context_data(self, **kwargs):
+        context = super(PersonDetailView, self).get_context_data(**kwargs)
+        person = Person.objects.get(id=self.kwargs['pk'])
+        data = dict()
+        data['name'] = person.name
+        data['id'] = person.id
+        data['start'] = person.start
+        data['stop'] = person.stop
+        data['role'] = person.role
+        data['studies'] = person.studies
+        print(person.studies)
+        data['full_id'] = person.full_id
+        context['person'] = data
+        return context
+
+
+class UpdateUser(generic.UpdateView):
+    model = Person
+    fields = ('name', 'start', 'stop', 'role', 'studies')
+    template_name_suffix = '/update-user'
+    # success_url = reverse_lazy('grades:user', args=(!!self.object.id!!))
+
+    # def get_context_data(self):
+    #     context = super(PersonDetailView, self).get_context_data(**kwargs)
+    #     person = Person.objects.get(id=self.kwargs['pk'])
+    #
+    #     data = dict()
+    #     data['id'] = person.id
+    #     context['person'] = data
+
+    def get_success_url(self):
+        return reverse_lazy('grades:user', args=(self.object.id,))
+
+    def get_absolute_url(self):
+        return u'/grades/user/%d' % self.id
+
+    # def get_object(self, queryset=None):
+    #     obj = Person.objects.get(id=self.kwargs['pk'])
+    #     return obj
