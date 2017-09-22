e3eaf7b1db53f2d10c3ac864c33ba5b011792bd9
Gerwin <gerwinputtenstein@gmail.com>
2017-09-22 20:02:17 +0200

    Added custom bootstrap. More work on the dashboard. Now two columns, one with active modules, other with all modules

--- Seth/dashboard/views.py
+++ Seth/dashboard/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from django.http import Http404
-from Grades.models import Module, Course
+from django.utils import timezone
+from Grades.models import Module, Course, Module_ed, Test
+
+import time
+import datetime
 
 # @permission_required('grades.add_grade')
 def home(request):
     context = {
-         'modules': get_modules()
+        'modules': get_modules(),
+        'time': get_current_date()
         # [
         #     {
         #         'name': 'Pearls of Computer Science',
@@ -20,10 +25,14 @@ def home(request):
     }
     return render(request, 'dashboard/index.html', context)
 
+def logged_out(request):
+    return render(request, 'registration/success_logged_out.html')
 
 def settings(request):
     raise Http404('Settings unimplemented')
 
-
 def get_modules():
-    return Module.objects.order_by('start')
+    return Module_ed.objects.order_by('module__name')
+
+def get_current_date():
+    return timezone.localdate()
