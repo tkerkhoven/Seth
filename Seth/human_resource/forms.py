e5172fbf111d55547eb9b24d2a84b3160f107835
Gerwin <gerwinputtenstein@gmail.com>
2017-09-26 12:18:13 +0200

    Removed role from the person form in resource management.

--- Seth/human_resource/forms.py
+++ Seth/human_resource/forms.py
@@ -4,4 +4,4 @@ from Grades.models import Person
 class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = Person
-        fields = ['name', 'id_prefix', 'person_id', 'start', 'stop', 'role',]
+        fields = ['name', 'id_prefix', 'person_id', 'start', 'stop',]
