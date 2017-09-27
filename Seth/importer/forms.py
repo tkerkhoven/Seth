06e955bacbac8cbf87a61debe875e450772e562a
JaspervanRooijen <jasper.van.rooijen@hotmail.com>
2017-09-27 17:14:42 +0200

    Added functionality to add new students to the system through the Importer

--- Seth/importer/forms.py
+++ Seth/importer/forms.py
@@ -13,3 +13,9 @@ class TestGradeUploadForm(forms.Form):
     file = forms.FileField(
         label='Select the graded excel file',
     )
+
+
+class ImportStudentForm(forms.Form):
+    file = forms.FileField(
+        label='Select Student excel file'
+    )
