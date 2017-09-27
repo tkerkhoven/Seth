45b61f20a0e866b26e846885e5e03d41edd2e7bf
Kevkev1 <kevin1226@live.nl>
2017-09-27 17:20:52 +0200

    Added colors + slider

--- Seth/static/Seth/seth/js/scripts.js
+++ Seth/static/Seth/seth/js/scripts.js
@@ -16,3 +16,49 @@ jQuery(document).ready(function($) {
         window.location = $(this).data("href");
     });
 });
+
+jQuery(document).ready(function($) {
+    $('[data-toggle="tooltip"]').tooltip();
+
+    var changeSlider = function(data) {
+      $('[id^="gradeid_"]').each(function(index) {
+        var grade = $(this).attr("data-grade")
+        if(+grade > +data.to) {
+          $(this).removeClass("success warning error")
+          $(this).addClass("success")
+        }
+        else if(+grade >= +data.from) {
+          $(this).removeClass("success warning error")
+          $(this).addClass("warning")
+        }
+        else if(+grade < +data.from) {
+          $(this).removeClass("success warning error")
+          $(this).addClass("error")
+        }
+      });
+    };
+
+    $("#colorslider").ionRangeSlider({
+                                    min: 1,
+                                    max: 10,
+                                    from: 5,
+                                    to: 5.5,
+                                    step: 0.1,
+                                    type: 'double',
+                                    grid: true,
+                                    grid_num: 9,
+                                    onChange: changeSlider
+                                });
+
+    $('[id^="gradeid_"]').each(function(index) {
+        if($(this).attr("data-grade") > 5.5) {
+            $(this).addClass("success")
+        }
+        else if($(this).attr("data-grade") >= 5) {
+            $(this).addClass("warning")
+        }
+        else if($(this).attr("data-grade") < 5) {
+            $(this).addClass("error")
+        }
+    });
+});
