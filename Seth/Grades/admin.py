3a25b166c44d45b719c2c96af57c33b04f39e755
Tim Kerkhoven <t.kerkhoven@student.utwente.nl>
2017-09-28 17:16:04 +0200

    added delete views, added property in models, added teacher in admin, formatted admin

--- Seth/Grades/admin.py
+++ Seth/Grades/admin.py
@@ -3,17 +3,29 @@ from django.contrib import admin
 # Register your models here.
 from Grades.models import *
 
+
 class CoordinatorAdmin(admin.ModelAdmin):
     list_display = ['person', 'module', 'mc_assistant']
 
+
 admin.site.register(Coordinator, CoordinatorAdmin)
+
 
 class GradeAdmin(admin.ModelAdmin):
     list_display = ('test_id', 'time', 'student_id', 'grade')
     list_filter = ['test_id', 'time', 'student_id', 'grade']
 
+
 admin.site.register(Grade, GradeAdmin)
+
+
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('person', 'course', 'role')
+    list_filter = ['person', 'course', 'role']
+
+
+admin.site.register(Teacher, TeacherAdmin)
+
 
 for object in [Person, Study, Studying, Module, Module_ed, Criterion, Course, Test]:
     admin.site.register(object)
-
--- Seth/module_management/views.py
+++ Seth/module_management/views.py
@@ -2,12 +2,14 @@ import pprint
 
 from django.core.exceptions import PermissionDenied, ValidationError
 from django.db.models import Q
+from django.db.transaction import set_autocommit, rollback, commit
 from django.forms.models import ModelForm
 from django.http import HttpResponseBadRequest
 from django.shortcuts import redirect
+from django.urls import reverse_lazy
 from django.views import generic
 
-from Grades.models import Module, Module_ed, Course, Test, Person, Coordinator
+from Grades.models import Module, Module_ed, Course, Test, Person, Coordinator, Teacher, Grade
 
 
 class IndexView(generic.ListView):
@@ -111,20 +113,20 @@ class ModuleEdUpdateView(generic.UpdateView):
         return handler(request, *args, **kwargs)
 
 
-class CreateModuleEdForm(ModelForm):
+class ModuleEdCreateForm(ModelForm):
     class Meta:
         model = Module_ed
         fields = ['module', 'module_code_extension', 'start', 'stop', 'year', 'module_coordinator', 'courses']
 
     def __init__(self, *args, **kwargs):
-        super(ModelForm, self).__init__(*args, **kwargs)
+        super(ModuleEdCreateForm, self).__init__(*args, **kwargs)
         self.fields['module'].widget.attrs['disabled'] = True
         self.fields['module_coordinator'].widget.attrs['disabled'] = True
 
 
 class ModuleEdCreateView(generic.CreateView):
     template_name = 'module_management/module_ed_create.html'
-    form_class = CreateModuleEdForm
+    form_class = ModuleEdCreateForm
 
     def get_initial(self):
         pk = self.request.path_info.split('/')[2]
@@ -156,6 +158,9 @@ class ModuleEdCreateView(generic.CreateView):
         data = self.get_form_kwargs()['data']
         pk = request.path_info.split('/')[2]
         latest_module_ed = Module_ed.objects.filter(module=pk).latest('year').pk
+
+        set_autocommit(False)
+
         module_ed = Module_ed(
             module=initial['module'],
             module_code_extension=data['module_code_extension'],
@@ -167,6 +172,8 @@ class ModuleEdCreateView(generic.CreateView):
             module_ed.full_clean()
         except ValidationError as e:
             pp = pprint.PrettyPrinter(indent=4, width=120)
+            rollback()
+            set_autocommit(True)
             return HttpResponseBadRequest(pp.pformat(('Form data is invalid: ', e.message_dict)))
         module_ed.save()
 
@@ -180,9 +187,13 @@ class ModuleEdCreateView(generic.CreateView):
                 coordinator.full_clean()
             except ValidationError as e:
                 pp = pprint.PrettyPrinter(indent=4, width=120)
+                rollback()
+                set_autocommit(True)
                 return HttpResponseBadRequest(pp.pformat(('Coordinator is invalid: ', e.message_dict)))
             coordinator.save()
 
+        commit()
+        set_autocommit(True)
         return redirect('module_management:module_overview')
 
 
@@ -229,6 +240,113 @@ class CourseUpdateView(generic.UpdateView):
         return handler(request, *args, **kwargs)
 
 
+class CourseCreateForm(ModelForm):
+    class Meta:
+        model = Course
+        fields = ['code', 'code_extension', 'name', 'teachers']
+
+
+class CourseCreateView(generic.CreateView):
+    template_name = 'module_management/course_create.html'
+    form_class = CourseCreateForm
+
+    def dispatch(self, request, *args, **kwargs):
+        user = request.user
+        pk = self.request.path_info.split('/')[2]
+
+        if not Person.objects.filter(coordinator__module=pk).filter(user=user):
+            raise PermissionDenied()
+
+        # Try to dispatch to the right method; if a method doesn't exist,
+        # defer to the error handler. Also defer to the error handler if the
+        # request method isn't on the approved list.
+        if request.method.lower() in self.http_method_names:
+            handler = getattr(self, request.method.lower(), self.http_method_not_allowed)
+        else:
+            handler = self.http_method_not_allowed
+        return handler(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        data = self.get_form_kwargs()['data']
+        pk = request.path_info.split('/')[2]
+
+        set_autocommit(False)
+
+        course = Course(
+            code=data['code'],
+            code_extension=data['code_extension'],
+            name=data['name']
+        )
+        try:
+            course.full_clean()
+        except ValidationError as e:
+            pp = pprint.PrettyPrinter(indent=4, width=120)
+            rollback()
+            set_autocommit(True)
+            return HttpResponseBadRequest(pp.pformat(('Form data is invalid: ', e.message_dict)))
+        course.save()
+
+        module_ed = Module_ed.objects.get(pk=pk)
+        module_ed.courses.add(course)
+        try:
+            module_ed.full_clean()
+        except ValidationError as e:
+            pp = pprint.PrettyPrinter(indent=4, width=120)
+            rollback()
+            set_autocommit(True)
+            return HttpResponseBadRequest(pp.pformat(('Module Edition is invalid: ', e.message_dict)))
+        module_ed.save()
+
+        for t in data.getlist('teachers'):
+            t = Person.objects.get(pk=t)
+            if t.id_prefix == 'm':
+                role = 'T'
+            else:
+                role = 'A'
+            teacher = Teacher(
+                course=course,
+                person=t,
+                role=role
+            )
+            try:
+                teacher.full_clean()
+            except ValidationError as e:
+                pp = pprint.PrettyPrinter(indent=4, width=120)
+                rollback()
+                set_autocommit(True)
+                return HttpResponseBadRequest(pp.pformat(('Teacher is invalid: ', e.message_dict)))
+            teacher.save()
+
+        commit()
+        set_autocommit(True)
+        return redirect(reverse_lazy('module_management:module_ed_detail', kwargs={'pk': pk}))
+
+
+class CourseDeleteView(generic.DeleteView):
+    model = Course
+    template_name = 'module_management/course_delete.html'
+    success_url = reverse_lazy('module_management:module_overview')
+
+    def dispatch(self, request, *args, **kwargs):
+        user = request.user
+        pk = self.request.path_info.split('/')[2]
+
+        if Grade.objects.filter(test_id__course_id=pk):
+            raise PermissionDenied('Cannot remove a course that has grades')
+
+        if not Course.objects.filter(Q(pk=pk) & (Q(module_ed__module_coordinator__user=user))):
+            raise PermissionDenied()
+
+        # Try to dispatch to the right method; if a method doesn't exist,
+        # defer to the error handler. Also defer to the error handler if the
+        # request method isn't on the approved list.
+        if request.method.lower() in self.http_method_names:
+            handler = getattr(self, request.method.lower(), self.http_method_not_allowed)
+        else:
+            handler = self.http_method_not_allowed
+        return handler(request, *args, **kwargs)
+
+
 class TestView(generic.DetailView):
     template_name = 'module_management/test_detail.html'
     model = Test
@@ -260,6 +378,96 @@ class TestUpdateView(generic.UpdateView):
         user = request.user
         if not Test.objects.filter(Q(pk=pk) & (
                     Q(course_id__module_ed__module_coordinator__user=user) | Q(course_id__teachers__user=user))):
+            raise PermissionDenied()
+
+        # Try to dispatch to the right method; if a method doesn't exist,
+        # defer to the error handler. Also defer to the error handler if the
+        # request method isn't on the approved list.
+        if request.method.lower() in self.http_method_names:
+            handler = getattr(self, request.method.lower(), self.http_method_not_allowed)
+        else:
+            handler = self.http_method_not_allowed
+        return handler(request, *args, **kwargs)
+
+
+class TestCreateForm(ModelForm):
+    class Meta:
+        model = Test
+        fields = ['course_id', 'name', '_type', 'time', 'maximum_grade', 'minimum_grade']
+
+    def __init__(self, *args, **kwargs):
+        super(TestCreateForm, self).__init__(*args, **kwargs)
+        self.fields['course_id'].widget.attrs['disabled'] = True
+
+
+class TestCreateView(generic.CreateView):
+    template_name = 'module_management/test_create.html'
+    form_class = TestCreateForm
+
+    def get_initial(self):
+        pk = self.request.path_info.split('/')[2]
+        return {
+            'course_id': Course.objects.get(pk=pk)
+        }
+
+    def dispatch(self, request, *args, **kwargs):
+        user = request.user
+        pk = self.request.path_info.split('/')[2]
+
+        if not Course.objects.filter(Q(pk=pk) & (Q(module_ed__module_coordinator__user=user))):
+            raise PermissionDenied()
+
+        # Try to dispatch to the right method; if a method doesn't exist,
+        # defer to the error handler. Also defer to the error handler if the
+        # request method isn't on the approved list.
+        if request.method.lower() in self.http_method_names:
+            handler = getattr(self, request.method.lower(), self.http_method_not_allowed)
+        else:
+            handler = self.http_method_not_allowed
+        return handler(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        initial = self.get_form_kwargs()['initial']
+        data = self.get_form_kwargs()['data']
+        pk = request.path_info.split('/')[2]
+
+        set_autocommit(False)
+
+        test = Test(
+            course_id=initial['course_id'],
+            name=data['name'],
+            _type=data['_type'],
+            time=data['time'],
+            maximum_grade=data['maximum_grade'],
+            minimum_grade=data['minimum_grade']
+        )
+        try:
+            test.full_clean()
+        except ValidationError as e:
+            pp = pprint.PrettyPrinter(indent=4, width=120)
+            rollback()
+            set_autocommit(True)
+            return HttpResponseBadRequest(pp.pformat(('Form data is invalid: ', e.message_dict)))
+        test.save()
+
+        commit()
+        set_autocommit(True)
+        return redirect(reverse_lazy('module_management:course_detail', kwargs={'pk': pk}))
+
+
+class TestDeleteView(generic.DeleteView):
+    model = Test
+    template_name = 'module_management/test_delete.html'
+    success_url = reverse_lazy('module_management:module_overview')
+
+    def dispatch(self, request, *args, **kwargs):
+        user = request.user
+        pk = self.request.path_info.split('/')[2]
+
+        if Grade.objects.filter(test_id=pk):
+            raise PermissionDenied('Cannot remove a test that has grades')
+
+        if not Test.objects.filter(Q(pk=pk) & (Q(course_id__module_ed__module_coordinator__user=user))):
             raise PermissionDenied()
 
         # Try to dispatch to the right method; if a method doesn't exist,
