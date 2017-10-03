0b0409d45803f31cc443ad0a63c7dc2de2c24464
Gerwin <gerwinputtenstein@gmail.com>
2017-10-03 10:05:34 +0200

    More work on redesign MM

--- Seth/module_management/views.py
+++ Seth/module_management/views.py
@@ -14,7 +14,7 @@ from Grades.models import Module, Module_ed, Course, Test, Person, Coordinator, 
 
 
 class IndexView(generic.ListView):
-    template_name = 'module_management/index.html'
+    template_name = 'module_management/index2.html'
     context_object_name = 'module_list'
 
     def get_queryset(self):
