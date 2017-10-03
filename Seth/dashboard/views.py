8fd7a61f7a12e4cd7713502ea2e66f8ff52db1fa
Tim Kerkhoven <t.kerkhoven@student.utwente.nl>
2017-10-03 17:00:24 +0200

    some tests

--- Seth/dashboard/views.py
+++ Seth/dashboard/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
 from django.http import Http404
+from django.shortcuts import render
 from django.utils import timezone
+
 from Grades.models import Module_ed
 
 
@@ -40,16 +41,16 @@ def get_current_date():
 
 
 def server_error(request):
-    return render(request, 'errors/500.html')
+    return render(request, 'errors/500.html', status=500)
 
 
 def not_found(request):
-    return render(request, 'errors/404.html')
+    return render(request, 'errors/404.html', status=404)
 
 
 def permission_denied(request):
-    return render(request, 'errors/403.html')
+    return render(request, 'errors/403.html', status=403)
 
 
 def bad_request(request):
-    return render(request, 'errors/400.html')
+    return render(request, 'errors/400.html', status=400)
