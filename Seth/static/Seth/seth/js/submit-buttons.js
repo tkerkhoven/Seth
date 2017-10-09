7b7c563f122bdb9e088a159036841d2371fd772d
Gerwin <gerwinputtenstein@gmail.com>
2017-10-09 11:28:17 +0200

    Finished styling of module management

--- Seth/static/Seth/seth/js/submit-buttons.js
+++ Seth/static/Seth/seth/js/submit-buttons.js
@@ -18,4 +18,28 @@ $(document).ready(function() {
     $("#createPersonSave").on('click', function() {
         $("#createPersonForm").submit();
     });
+
+    $("#createModuleEdSave").on('click', function() {
+        $("#createModuleEdForm").submit();
+    });
+
+    $("#updateModuleEdSave").on('click', function() {
+        $("#updateModuleEdForm").submit();
+    });
+
+    $("#createModulePartSave").on('click', function() {
+        $("#createModulePartForm").submit();
+    });
+
+    $("#updateModulePartSave").on('click', function() {
+        $("#updateModulePartForm").submit();
+    });
+
+    $("#createTestSave").on('click', function() {
+        $("#createTestForm").submit();
+    });
+
+    $("#updateTestSave").on('click', function() {
+        $("#updateTestForm").submit();
+    });
 });
