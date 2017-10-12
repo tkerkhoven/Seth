22248fc139c4672895b9eaf1153a8fba41e6066f
Martijn Verkleij <github@mverkleij.nl>
2017-10-12 11:18:39 +0200

    RADIUS remove password save again

--- Seth/Seth/settings.py
+++ Seth/Seth/settings.py
@@ -142,6 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/html/static/'
 STATICFILES_DIRS = [
     'static/Seth'
 ]
