2efe8e70ca1cfcf505180dffdeb048a79a0b0b61
Gerwin <gerwinputtenstein@gmail.com>
2017-10-18 09:57:11 +0200

    Stuff

--- Seth/human_resource/views.py
+++ Seth/human_resource/views.py
@@ -34,7 +34,7 @@ def known_persons(person):
         studies = Study.objects.none()
         for mod in modules:
             study_set = Study.objects.filter(modules=mod)
-            studies = (studies | study_set).distinct()
+            studies = (studies.all() | study_set.all())
         advisers = Person.objects.filter(study__in=studies)
         queryset = queryset.union(queryset, advisers)
         # Add Teachers and Teaching Assistants
