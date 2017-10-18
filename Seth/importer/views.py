c2fe8a5090c74d71d42403c388383fdec4e0b5a6
JaspervanRooijen <jasper.van.rooijen@hotmail.com>
2017-10-18 15:52:29 +0200

    Index-fix 2

--- Seth/importer/views.py
+++ Seth/importer/views.py
@@ -642,8 +642,7 @@ def import_student_to_module(request, pk):
             string = ""
             emailpattern = re.compile('e[-]?mail*')
             if students_to_module[0][0].lower() == 'student_id' and students_to_module[0][
-                1].lower() == 'name' and emailpattern.match(students_to_module[0][2].lower()) and students_to_module[0][3].lower() == 'study' and \
-                            students_to_module[0][4].lower() == 'role':
+                1].lower() == 'name' and emailpattern.match(students_to_module[0][2].lower()) and students_to_module[0][3].lower() == 'role':
                 context = {}
                 context['created'] = []
                 context['studying'] = []
