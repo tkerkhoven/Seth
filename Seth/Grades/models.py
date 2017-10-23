ba88001a0ba3baffcff0e4b49496be07fdb5adca
Martijn Verkleij <github@mverkleij.nl>
2017-10-23 10:40:06 +0200

    Importer: Changed workbooks to include minimum and maximum grade. Changed string in workbooks: 'student_id' to 'university_number'

--- Seth/Grades/models.py
+++ Seth/Grades/models.py
@@ -79,7 +79,7 @@ class ModuleEdition(models.Model):
 
     @property
     def code(self):
-        return '{}-{}-{}'.format(self.year, self.module.code, self.block)
+        return '{}-{}-{}'.format(self.module.code, self.year, self.block)
 
     # Should be the same as BLOCKS
     @property
