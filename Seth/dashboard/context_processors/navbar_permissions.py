7d36a15836b50b2e602496a3c550807fd0a610a4
Gerwin <gerwinputtenstein@gmail.com>
2017-10-24 17:16:18 +0200

    Working on the permissions for the navbar, not working on all pages for some reason

--- Seth/dashboard/context_processors/navbar_permissions.py
+++ Seth/dashboard/context_processors/navbar_permissions.py
@@ -1,10 +1,29 @@
-from Grades.models import Coordinator
+from Grades.models import Coordinator, Teacher, Study
 
 
 def permissions(request):
 
-    if not request.user.is_anonymous() and Coordinator.objects.filter(person__user=request.user):
+    # if not request.user.is_anonymous() and Coordinator.objects.filter(person__user=request.user):
+    #
+    #     return {'module_coordinator': True}
+    # else:
+    #     return {'module_coordinator:': False}
+    context = dict()
 
-        return {'module_coordinator': True}
-    else:
-        return {'module_coordinator:': False}
+    if not request.user.is_anonymous():
+        context['is_module_coordinator'] = False
+        context['is_teacher'] = False
+        context['is_ta'] = False
+        context['is_mc_assistant'] = False
+        context['is_adviser'] = False
+        if Coordinator.objects.filter(person__user=request.user):
+            context['is_module_coordinator'] = True
+        if Teacher.objects.filter(person__user=request.user).filter(role="Teacher"):
+            context['is_teacher'] = True
+        if Teacher.objects.filter(person__user=request.user).filter(role="Teaching Assistant"):
+            context['is_ta'] = True
+        if Coordinator.objects.filter(person__user=request.user).filter(is_assistant=True):
+            context['is_mc_assistant'] = True
+        if Study.objects.filter(advisers__user=request.user):
+            context['is_adviser'] = True
+    return context
